83bd8ad3d952f5990a3420e8b872f435c0363837
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-04-17 08:01:05 -0600

    Update TestSpace.py

--- ROADMAP_BACK3NDLAND/PYTHON/100DaysOfPyton/Day 2/TestSpace.py
+++ ROADMAP_BACK3NDLAND/PYTHON/100DaysOfPyton/Day 2/TestSpace.py
@@ -20,9 +20,33 @@ else:
 
 """
 #print(Len("hello")--> 4
+
+
 #Data Types 
 #Strings "Hello" [0]
 print("Hello" [0])
+
+#Integer
+primt(123+123)
+
+#Float
+print(3.2)
+
+#Boolean
+print(True, False)
+
+#len() Does not like to work with Int
+num_char= len(input("Hello World"))
+print("Your name has " + num_char + "characters.")
+
+
+b = float(1)
+a = "234"
+print(f"el dato {b} + {a} es igual {float(a)+ b}")
+
+
+
+
 
 
 
