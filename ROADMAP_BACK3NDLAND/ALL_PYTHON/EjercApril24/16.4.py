213def0744adc3926b635fa3e9be7376a9051811
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-04-16 21:09:58 -0600

    Update 16.4.py

--- ROADMAP_BACK3NDLAND/ALL_PYTHON/EjercApril24/16.4.py
+++ ROADMAP_BACK3NDLAND/ALL_PYTHON/EjercApril24/16.4.py
@@ -2,3 +2,10 @@ print(len("Hello"))
 print("Hola developers vamos a crear ejercicios practicos para BACKENDLAND!!")
 
 print("Hola developers vamos a crear ejercicios")
+
+"""
+
+Comentarios de varias lineas
+
+
+""" 
