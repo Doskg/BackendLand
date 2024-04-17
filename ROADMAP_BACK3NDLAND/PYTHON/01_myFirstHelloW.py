287a9b4a241ad7f781731cfbe303f7835518e645
Oscar Gonzalez <oscar.gonzalez.baldizon@gmail.com>
2024-04-16 21:21:09 -0600

    new message

---
+++ ROADMAP_BACK3NDLAND/PYTHON/01_myFirstHelloW.py
@@ -0,0 +1,32 @@
+print("Hello World")
+
+# este es un comentario de una sola linea"
+
+"""
+Este
+es 
+un
+comentario
+de
+multiples
+lineas"""
+print(type("Cual es mi tipo de dato"))
+
+print(type(20))
+print(type(1.5))
+# estamos probando la consola de PYTHON
+print("estamos probando la consola de Python1")
+print("estamos probando la consola de Python2")
+print("estamos probando la consola de Python3")
+print("estamos probando la consola de Python4")
+print("estamos probando la consola de Python5")
+
+a = 2
+
+print(a)
+b = 3
+c = a + b
+
+print(c)
+
+# esto es un espacio en blanco
