33ecbc0c5d460168fd6fd3066502150f393eeee4
Oscar Gonzalez <oscar.gonzalez.baldizon@gmail.com>
2024-05-15 20:42:33 -0600

    a

--- ROADMAP_BACK3NDLAND/PYTHON/May/15LogicaProgra.py
+++ ROADMAP_BACK3NDLAND/PYTHON/May/15LogicaProgra.py
@@ -1,6 +1,5 @@
 """
 Condicionales
-  """
 num = 4
 if num >3: #si el numero es mayor a 3
     print("El numero es mayor a 3")
@@ -9,3 +8,8 @@ else:
       print("El numero es igual a 3")
     else:
       print("El numero es menor a 3") 
+  """
+n1 = 0
+while n1==7:
+  print(n1)
+  n1+=1
